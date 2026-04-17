0786b4385ba82f97eb977cd703a6bf7899f1a4fa
Bin0917 <leesd132@naver.com>
2026-04-17 14:41:20 +0900

    chore: userClient Interface로 변경 및 포트번호 수정 #13

--- offering-service/src/main/java/com/hanaro/offeringservice/client/user/UserClient.java
+++ offering-service/src/main/java/com/hanaro/offeringservice/client/user/UserClient.java
@@ -2,15 +2,14 @@ package com.hanaro.offeringservice.client.user;
 
 import com.hanaro.offeringservice.dto.UsePointRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
     name = "user-service",
-    url = "http://user-service:8080"
+    url = "http://user-service:8083"
 )
-public class UserClient {
-  @PostMapping("/internal/users/points/use")
-  void usePoint(@RequestBody UsePointRequest request);
+public interface UserClient {
+    @PostMapping("/internal/users/points/use")
+    void usePoint(@RequestBody UsePointRequest request);
 }
