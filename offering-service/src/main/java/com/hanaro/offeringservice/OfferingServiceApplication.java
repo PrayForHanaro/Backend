738730de9770af9579792a9c58b9437a1ad3399d
Bin0917 <leesd132@naver.com>
2026-04-17 14:41:56 +0900

    chore: feign 사용을 위해 application에 어노테이션 추가 #13

--- offering-service/src/main/java/com/hanaro/offeringservice/OfferingServiceApplication.java
+++ offering-service/src/main/java/com/hanaro/offeringservice/OfferingServiceApplication.java
@@ -2,7 +2,9 @@ package com.hanaro.offeringservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = "com.hanaro")
 public class OfferingServiceApplication {
 
