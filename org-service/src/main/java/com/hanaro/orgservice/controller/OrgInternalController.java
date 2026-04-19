0229c0a29dbf2ec32741a58b4e4d42fce27acef3
typeyu <mytypeyu@gmail.com>
2026-04-20 02:40:49 +0900

    refactor: auth #2

--- org-service/src/main/java/com/hanaro/orgservice/controller/OrgInternalController.java
+++ org-service/src/main/java/com/hanaro/orgservice/controller/OrgInternalController.java
@@ -1,8 +1,11 @@
 package com.hanaro.orgservice.controller;
 
 import com.hanaro.common.response.ApiResponse;
+import com.hanaro.common.security.CustomUserDetails;
+import com.hanaro.orgservice.dto.OrgMyPageResponseDTO;
 import com.hanaro.orgservice.service.OrgService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class OrgInternalController {
 
     private final OrgService orgService;
+
+    @GetMapping("/me")
+    public OrgMyPageResponseDTO getMyOrg(
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        return orgService.getMyOrg(user.getOrgId());
+    }
 
     @GetMapping("/{orgId}/summary")
     public ApiResponse getOrgSummaryInternal(@PathVariable Long orgId) {
