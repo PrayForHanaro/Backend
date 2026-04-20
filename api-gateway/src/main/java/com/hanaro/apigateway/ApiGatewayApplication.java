0089aa347f4193174bac994480396fc8de28836f
Jeongsu <latsyrc900@gmail.com>
2026-04-20 10:17:00 +0900

    fix: api-gateway의 jwt인증

--- api-gateway/src/main/java/com/hanaro/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hanaro/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,8 @@ package com.hanaro.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.hanaro.apigateway", "com.hanaro.common"})
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
--- api-gateway/src/main/java/com/hanaro/apigateway/filter/GatewayUserHeaderFilter.java
+++ api-gateway/src/main/java/com/hanaro/apigateway/filter/GatewayUserHeaderFilter.java
@@ -17,8 +17,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.ServerRequest;
 
+@Component
 @Configuration
 public class GatewayUserHeaderFilter {
 
